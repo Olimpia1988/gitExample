d5d0a02c1c2ff18f245349d827d3302853ddb2f8
Olimpia <olimpiacompean@prusuit.org>
2019-02-08 10:55:08 -0500

    first commit

---
+++ GitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExample
+//
+//  Created by Olimpia on 2/8/19.
+//  Copyright © 2019 Olimpia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("Working on git")
+    }
+
+
+}
+
